3cfe3f4a573a0951117958904e9c57bbf6551a43
fortune <basket85r@gmail.com>
2024-03-28 17:30:06 +0900

    getMedai関数追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
-function App() {
+const getMedia = async () => {
+  const constraints = {
+    video: true,
+    audio: true,
+  };
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+    /* ストリームを使用 */
+  } catch (err) {
+    /* エラーを処理 */
+    console.log(err);
+  }
+}
+
+getMedia();
+
+const App = () => {
   return <div>Hello, React!</div>;
 }
 
