7906b9b630e66844a79b595deb4a568525991e04
fortune <basket85r@gmail.com>
2024-04-30 18:04:53 +0900

    43 firebaseが発行するキーをgit commit対象から外した

--- src/utils/FirebaseSignallingClient.js
+++ src/utils/FirebaseSignallingClient.js
@@ -3,14 +3,24 @@ import 'firebase/compat/database';
 
 export default class FirebaseSignallingClient {
   constructor() {
+    const {
+      REACT_APP_FIREBASE_API_KEY,
+      REACT_APP_FIREBASE_AUTH_DOMAIN,
+      REACT_APP_FIREBASE_DATABASE_URL,
+      REACT_APP_FIREBASE_PROJECT_ID,
+      REACT_APP_FIREBASE_STORAGE_BUCKET,
+      REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      REACT_APP_FIREBASE_APP_ID,
+    } = process.env;
+
     const firebaseConfig = {
-      apiKey: "AIzaSyDvHLkRZ3aKSnuz8y3KUM8Dq3_qycJ3qbk",
-      authDomain: "webrtc-tutorial-37e06.firebaseapp.com",
-      databaseURL: "https://webrtc-tutorial-37e06-default-rtdb.firebaseio.com",
-      projectId: "webrtc-tutorial-37e06",
-      storageBucket: "webrtc-tutorial-37e06.appspot.com",
-      messagingSenderId: "591419662104",
-      appId: "1:591419662104:web:4167f34f85fdcdc97c886d"
+      apiKey: REACT_APP_FIREBASE_API_KEY,
+      authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+      databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+      projectId: REACT_APP_FIREBASE_PROJECT_ID,
+      storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+      appId: REACT_APP_FIREBASE_APP_ID,
     };
     if(firebase.apps.length === 0) firebase.initializeApp(firebaseConfig);
     this.database = firebase.database();
